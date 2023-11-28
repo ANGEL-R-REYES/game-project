91b32e77e96fff2cd37ded0336353648c6e2981d
TheHiddenError <148292099+TheHiddenError@users.noreply.github.com>
2023-11-27 23:41:45 -0600

    Final EventHandler

--- EventHanlder.py
+++ EventHanlder.py
@@ -4,43 +4,39 @@ import random
 
 from Character import*
 
+
+
+
 #you can insert music and sounds in this class
 
 class Event:
-    def __init__(self, window, boss, main):
-        self.window = window
-        self.x = self.window.get_width()
-
-        self.scroll = 0
+    def __init__(self, window, main):
+        self.window = window                                #for game screen
+        
+        self.scroll = 0                                     #for scrolling background purposes
         self.scroll_speed = 5
 
-        self.flying_y = 375
+        self.x = self.window.get_width()
+        self.flying_y = 375                                 #locations and speeds of the enemies
         self.ground_y = 425
         self.vel = 3
 
-        self.active_wave = False
+        self.active_wave = False                            #keeps track of waves and holds the current enemies of wave
         self.waves = 0
         self.spawn_cooldown = 0
         self.enemies = []
 
-        self.score = 0
+        self.score = 0                                      #to keep track and display score
         self.score_cooldown = 0
         self.font = pygame.font.SysFont("arial", 50, True)
-        
-        self.superCharge = pygame.Rect(600, self.flying_y - 100, 50, 50)
-        self.powerup_cooldown = 1000
-        self.powerup_active = False
 
-        self.boss = boss
-        self.boss_cooldown = 0
-        self.boss_active = False
 
-        self.faster_cooldown = 1000
+        self.faster_cooldown = 1000                         #to prevent the speed from changing to frequently
 
-        self.start = False
+        self.start = False                                  #to know when the game is over and when it starts
         self.game_over = False
 
-        self.main = main
+        self.main = main                                    #for sprite collision
         self.mainGroup = pygame.sprite.Group()
         self.mainGroup.add(self.main)
 
@@ -54,8 +50,19 @@ class Event:
                 pygame.quit()
                 exit()
 
+
     def startScreen(self):              #insert start screen code here
         self.window.fill((0,255,0))
+        self.start_text = self.font.render(f"Welcome to endless runner!", True, (255,255,255), None)
+        self.start_text2 = self.font.render(f"Press 0 to Start!", True, (255,255,255), None)
+        textRect = self.start_text.get_rect()
+        textRect2 = self.start_text2.get_rect()
+        textRect.center = (400,50)
+        textRect2.center = (400, 150)
+        self.window.blit(self.start_text, textRect)
+        self.window.blit(self.start_text2, textRect2)
+
+
         keys = pygame.key.get_pressed()
         self.main.action = 5
         self.main.offset[1] = 50
@@ -86,13 +93,12 @@ class Event:
 
 
         ground_width = self.ground_image.get_width()
-        ground_height = self.ground_image.get_height()
 
-        for x in range(4):
+        for z in range(4):
             speed = 1
             for i in self.bg_images:
-                self.window.blit(i, ((x * bg_width + self.scroll * speed),0))
-                speed +=.2
+                self.window.blit(i, ((z * bg_width + self.scroll * speed),0))
+                speed += .2
 
         for y in range(15):
             self.window.blit(self.ground_image, ((y * ground_width) + self.scroll * 2.5, 550))
@@ -112,16 +118,22 @@ class Event:
     def spawnEnemeies(self):        #creates and draws the enemies 
         if self.active_wave == False:
             return
-        elif self.boss_active != True:
-            if len(self.enemies) < 4 and self.spawn_cooldown == 0:
-                random_number = random.randrange(0,2)
-                if random_number == 0:
-                    self.enemies.append(flyingEnemies(self.x + random.randrange(0,250, 50), self.flying_y))
-                else:
-                    self.enemies.append(groundEnemies(self.x + random.randrange(0,250, 50), self.ground_y))
-                self.spawn_cooldown = 100
-            else: 
-                return
+        if len(self.enemies) < 4 and self.spawn_cooldown == 0:
+            random_number = random.randrange(0,4)
+            if random_number == 0:
+                self.enemies.append(flyingEnemies(self.x + random.randrange(0,250, 50), self.flying_y))
+            elif random_number == 1:
+                self.enemies.append(groundEnemies(self.x + random.randrange(0,250, 50), self.ground_y))
+            elif random_number == 2:
+                self.enemies.append(flyingEnemies(self.x + random.randrange(0,250, 50), self.flying_y))
+                self.enemies.append(flyingEnemies(self.x + random.randrange(0,250, 50), self.flying_y -150))
+            else:
+                self.enemies.append(groundEnemies(900, self.ground_y))
+                self.enemies.append(groundEnemies(1000, self.ground_y))
+
+            self.spawn_cooldown = 100
+        else: 
+            return
 
     def check(self):        #check to see if enemies are out of the screen. If yes, then it would remove them from list
         checker = 0
@@ -137,19 +149,25 @@ class Event:
             bottomr, _ = enemy.rect.bottomright
             if bottomr > 0:
                 self.check_enemy_collision(enemy)
-                # enemy.loop()
+                self.handle_enemy_attack(enemy)
                 enemy.rect.x -= self.vel * 2
-                # pygame.draw.rect(self.window, (255,0,0), enemy.rect)
+            
             else:
                 self.enemies.remove(enemy)
 
     def check_enemy_collision(self, enemy):
         if pygame.Rect.colliderect(enemy.rect, self.main.rect):
             if pygame.sprite.spritecollide(enemy, self.mainGroup, False):
-                self.game_over = True       #game would force quit if the main rect collides with enemy rect. For testing purposes
-                print("Game Over")
+                self.main.health -=100
+                if self.main.health == 0:
+                    self.game_over = True    
+                    print("Game Over")
 
-
+    def handle_enemy_attack(self, enemy):
+        if enemy.rect.x - self.main.rect.x < 125 and self.main.rect.centery - enemy.rect.centery < 75:
+            enemy.attacking = True
+        else:
+            enemy.attacking = False
 
     def scoreBoard(self):
         if self.score_cooldown == 0:
@@ -173,57 +191,27 @@ class Event:
         if self.score_cooldown > 0:
             self.score_cooldown -=1
 
-        if self.boss_cooldown > 0:
-            self.boss_cooldown -= 1
-
         if self.faster_cooldown > 0:
             self.faster_cooldown -= 10
 
-        if self.powerup_cooldown > 0:
-            self.powerup_cooldown -= 5
-        elif self.powerup_cooldown == 0:
-            self.powerup_active = True
-
-            
-
-
-
-    def check_collision(self):
-        if pygame.Rect.colliderect(self.superCharge, self.main.rect):
-            self.powerup_active = False
-            self.powerup_cooldown = 1000
 
     
     def draw(self):
         for enemy in self.enemies:
             enemy.update_sprite()
             enemy.draw(self.window)
+        self.main.update_sprite()
         self.main.draw(self.window)
-        if self.boss_active == True:
-            self.boss.draw(self.window)
 
     def goFaster(self):
 
         if self.score % 20 == 0 and self.faster_cooldown == 0:
             self.faster_cooldown = 100
-            self.vel += .2
-            self.scroll_speed += .1
+            self.vel += .5
+            self.scroll_speed += .3
 
-    def spawnBoss(self):
-        pygame.draw.rect(self.window, (255,0,0), self.boss.rect)
+
 
     
-    def handleBoss(self):            #Dont worry about this for now
-        if self.score > 300:
-            self.spawnBoss()
-            self.boss_active = True
-            self.boss.update_sprite()
-            if self.boss_cooldown == 0:
-                self.boss_cooldown = 300
-                self.boss.chooseAttack()
-            if self.boss.warning == True or self.boss.ground_fire == True:
-                
-                pygame.draw.rect(self.window, (255,0,0), self.boss.ground_attack_rect)
-
 
 
