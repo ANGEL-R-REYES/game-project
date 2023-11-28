52b85dc70e2f3588f57e1d1e866c65d7d599a87f
TheHiddenError <148292099+TheHiddenError@users.noreply.github.com>
2023-11-27 23:40:37 -0600

    Final Characters.py

--- Characters.py
+++ Characters.py
@@ -1,31 +1,25 @@
-import pygame
-import os
+import pygame 
 
-import random
-
-import math
 
 from os import listdir
 from os.path import isfile, join
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, x, y, info):       #ignore most of these since it is for animation
+    def __init__(self, x, y, info):      
         pygame.sprite.Sprite.__init__(self)
-        self.rect = pygame.Rect(x, y, 100, 160)                 #< -- focus here
+        self.rect = pygame.Rect(x, y, 100, 160)                 
         self.mask = None
         self.x,self.y = x, y
-        self.action = 0         #0: idle, 1: run, 2: attack1, 3: jump, 4: range attack, 5: hit, 6: death
+        self.action = 0         
         self.frame = 0 
         self.size_width = info[0]
         self.size_height = info[1]
         self.image_scale = info[2]
-        # self.offset = [60, 62]
         self.character = {}
-        # self.animation = self.load_images(spriteSheet, animation_steps)
 
-        self.vel = 3.5                                      #< -- and this section here
+        self.vel = 3.7                                     
         self.x_vel = 0
-        self.GRAVITY = .5 
+        self.GRAVITY = .5
         self.y_vel = 0
         self.jumpHeight = 0
 
@@ -34,9 +28,6 @@ class Character(pygame.sprite.Sprite):
         self.cooldown = 100
 
         self.health = 100
-        self.hit = False
-        self.stunned_cooldown = 0
-        self.Alive = True
 
 
         self.running = False
@@ -44,33 +35,8 @@ class Character(pygame.sprite.Sprite):
         self.attacking = False
 
 
-    def flip(self, sprites):        #to flip the sheets to face both directions
+    def flip(self, sprites):        #to flip the sheets
         return[pygame.transform.flip(sprite, True, False) for sprite in sprites]
-    
-    def loadImages(self, dir1, dir2, width, height, changeDirect):          #This function loads the images of the characters from spritesheet
-        
-        path = join(dir1, dir2)
-        images = [f for f in listdir(path) if isfile(join(path, f))]
-    
-        for image in images:
-            sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
-
-            sprites = []
-
-            for i in range(sprite_sheet.get_width()// width):
-                surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
-                rect =  pygame.Rect(i * width, 0, width, height)
-                surface.blit(sprite_sheet, (0,0), rect)
-                sprites.append(pygame.transform.scale2x(surface))
-                # sprites.append((surface))
-
-            if changeDirect:                                               #To have the sprite have options to face both directions
-                self.character[image.replace(".png", "") + "_right"] = sprites
-                self.character[image.replace(".png", "") + "_left"] = self.flip(sprites)
-            
-            else:                                                           #This would have sprite only face one direction
-                self.character[image.replace(".png", "")] = sprites
-
 
     
     def load_sheet(self, sprite_sheet, animation_steps, flip = False):
@@ -83,13 +49,11 @@ class Character(pygame.sprite.Sprite):
                 if flip:
                     temp_img = pygame.transform.flip(temp_img, True, False)
                 temp_img_list.append(pygame.transform.scale(temp_img, (self.size_width * self.image_scale, self.size_height * self.image_scale)))
-                # temp_img_list = self.flip(temp_img_list)
             animation_list.append(temp_img_list)
 
-        # animation_list = self.flip(animation_list)
         return animation_list
 
-    def loadImages(self, dir1, dir2, width, height, changeDirect):          #This function loads the images of the characters from spritesheet
+    def loadImages(self, dir1, dir2, width, height, changeDirect):          #This function loads the images of the characters from individual sheets
         
         path = join(dir1, dir2)
         images = [f for f in listdir(path) if isfile(join(path, f))]
@@ -104,25 +68,14 @@ class Character(pygame.sprite.Sprite):
                 rect =  pygame.Rect(i * width, 0, width, height)
                 surface.blit(sprite_sheet, (0,0), rect)
                 sprites.append(pygame.transform.scale_by(surface, 3))
-                # sprites.append((surface))
 
             if changeDirect:                                               #To have the sprite have options to face both directions
-                # self.character[image.replace(".png", "") + "_right"] = sprites
                 self.character[image.replace(".png", "") + "_left"] = self.flip(sprites)
             
             else:                                                           #This would have sprite only face one direction
                 self.character[image.replace(".png", "")] = sprites
 
 
-
-    
-    def draw(self, window, sprite):
-        window.blit(sprite, (self.rect.x - (self.offset[0] * self.image_scale), self.rect.y - (self.offset[1] * self.image_scale)))
-        # window.blit(self.animation[self.action][self.frame], (self.rect.x, self.rect.y))
-
-        
-
-    
 
 class theMain(Character):
     def __init__(self, x, y):
@@ -132,21 +85,10 @@ class theMain(Character):
         self.ducking = False
         self.ducking_cooldown = 0
         self.offset = [50, 10]
-        # self.currentSprite = "Run"
         super().__init__(x, y, data)
         self.animation = super().load_sheet(main, animation_steps)
         self.rect = pygame.Rect(x, y, 110, 135)
         
-
-    def draw(self, window):
-        super().draw(window)
-
-    def jump(self):
-        if self.stunned_cooldown == 0:
-            self.jumping = True
-
-    # def attack(self):
-    #     super().attack()
 
     def loop(self):
         
@@ -195,184 +137,22 @@ class theMain(Character):
         self.rect.x += self.x_vel       #updates the rect of character
         self.rect.y += self.y_vel
 
-        # super().loop()
 
 
     def update_sprite(self):
-        if self.ducking == True or self.jumping == True:
-            self.running = False
+        if self.health !=0:
+            if self.ducking == True or self.jumping == True:
+                self.running = False
 
-        if self.running:
-            self.action = self.change_animation(self.action, 0)
-        elif self.jumping:
-            self.action = self.change_animation(self.action, 6)
-        elif self.attacking:
-            self.action = self.change_animation(self.action, 3)
-        elif self.ducking:
-            self.action = self.change_animation(self.action, 2)
+            if self.running:
+                self.action = self.change_animation(self.action, 0)
+            elif self.jumping:
+                self.action = self.change_animation(self.action, 6)
+            elif self.attacking:
+                self.action = self.change_animation(self.action, 3)
+            elif self.ducking:
+                self.action = self.change_animation(self.action, 2)
 
-        sprites = self.animation[self.action]
-
-        if pygame.time.get_ticks() - self.timeUpdate > self.cooldown:       #changes frame based on a how much time has passed
-            self.timeUpdate = pygame.time.get_ticks()
-            self.frame += 1
-
-        if self.attacking == True and self.frame == len(self.animation[self.action]):  #this helps show the full attacking animation
-            self.attacking = False
-            
-        if self.frame >= len(self.animation[self.action]):     #once the frame goes through the last frame of the current action spritesheet, it would start again in the beginning. This to prevent the frame from going out of bounds 
-            if self.health == 0:
-                self.Alive = False
-            self.frame = 0
-
-        self.sprite = sprites[self.frame]
-
-
-        # self.rect = self.sprite.get_rect(topleft = (self.rect.x, self.rect.y))  #updates the rectangle of sprite
-        self.mask = pygame.mask.from_surface(self.sprite) 
-
-
-    def change_animation(self, current, desired):       #to change the action. If the desired action is the same as current, it would just leave since nothing needs to be done
-
-        if current == desired:
-            return current
-        
-        elif self.stunned_cooldown == 0:
-            current = desired
-            self.frame = 0
-
-        return current
-
-
-    def draw(self, window):
-        
-        window.blit(self.animation[self.action][self.frame], (self.rect.x - self.offset[0], self.rect.y - self.offset[1]))
-        # window.blit(self.animation[self.action][self.frame], (self.rect.x, self.rect.y))
-
-    
-
-class groundEnemies(Character):
-    def __init__(self, x, y):
-        self.currentSprite = "Walk"
-        self.direction = "left"
-        data = [160, 128, 3]
-        super().__init__(x, y, data)
-        super().loadImages("Characters", "Ground_Enemy", 150, 150, True)
-        self.rect = pygame.Rect(x, y, 90, 150)
-
-
-    
-    def update_sprite(self):
-        if self.health > 0:
-            self.running = True
-
-        image_sheet_name =  self.currentSprite + "_" + self.direction
-        sprites = self.character[image_sheet_name]
-
-        
-        if pygame.time.get_ticks() - self.timeUpdate > self.cooldown:       #changes frame based on a how much time has passed
-            self.timeUpdate = pygame.time.get_ticks()
-            self.frame += 1
-        
-        if self.frame >= len(self.character[image_sheet_name]):     #once the frame goes through the last frame of the current action spritesheet, it would start again in the beginning. This to prevent the frame from going out of bounds 
-            if self.health == 0:
-                self.Alive = False
-            self.frame = 0
-
-        self.sprite = sprites[self.frame]
-
-
-    def draw(self, window):
-        window.blit(self.sprite, (self.rect.x - 155, self.rect.y - 155))
-        # window.blit(self.animation[self.action][self.frame], (self.rect.x, self.rect.y))
-
-class flyingEnemies(Character):
-    def __init__(self, x, y):
-        self.currentSprite = "Flight"
-        self.direction = "left"
-        data = [160, 128, 3]
-        super().__init__(x, y, data)
-        super().loadImages("Characters", "Flying_Enemy", 150, 150, True)
-        self.rect = pygame.Rect(x, y, 100, 100)
-
-    def update_sprite(self):
-        if self.health > 0:
-            self.running = True
-
-        image_sheet_name = self.currentSprite + "_" + self.direction
-        sprites = self.character[image_sheet_name]
-
-        
-        if pygame.time.get_ticks() - self.timeUpdate > self.cooldown:       #changes frame based on a how much time has passed
-            self.timeUpdate = pygame.time.get_ticks()
-            self.frame += 1
-        
-        if self.frame >= len(self.character[image_sheet_name]):     #once the frame goes through the last frame of the current action spritesheet, it would start again in the beginning. This to prevent the frame from going out of bounds 
-            if self.health == 0:
-                self.Alive = False
-            self.frame = 0
-
-        self.sprite = sprites[self.frame]
-
-    def draw(self, window):
-        window.blit(self.sprite, (self.rect.x - 160, self.rect.y - 175))
-        # window.blit(self.animation[self.action][self.frame], (self.rect.x, self.rect.y))
-
-
-
-
-
-class Boss(Character):                      #ignore this class. It is not being used right now.
-    def __init__(self, x, y):
-        neo = pygame.image.load("Characters/Boss/Necromancer_creativekind-Sheet.png").convert_alpha()
-        animation_steps = [8, 8, 13, 13, 17, 5, 10]
-        data = [160, 128, 3]
-        super().__init__(x, y, data)
-        self.animation = super().load_sheet(neo, animation_steps, True)
-        self.ground_attack_cooldown = 0
-        self.ground_attack_rect = pygame.Rect(0, 500, 800, 100)
-        self.high_attack_cooldown = 0
-        self.high_attack_rect = pygame.Rect(0, 250, 800, 100)
-        self.draw_cooldown = 0
-        self.warning = False
-        self.warning_shots = 0
-        self.warning_cooldown = 0
-        self.ground_fire = False
-
-    def chooseAttack(self):
-        randomAtt = random.randrange(0,2)
-        if randomAtt == 0:
-            self.groundAttack()
-            
-
-        elif randomAtt == 1:
-            self.highAttack()
-
-
-    def groundAttack(self):
-        pass
-
-    def highAttack(self):
-        pass
-
-    def loop(self):
-        if self.ground_attack_cooldown > 0:
-            self.ground_attack_cooldown -= 1
-
-        if self.high_attack_cooldown > 0:
-            self.high_attack_cooldown -= 1
-
-        if self.draw_cooldown > 0:
-            self.draw_cooldown -= 1
-
-    def update_sprite(self):
-
-        if self.running:
-            self.action = self.change_animation(self.action, 1)
-        elif self.jumping:
-            self.action = self.change_animation(self.action, 6)
-        elif self.attacking:
-            self.action = self.change_animation(self.action, 3)
         else:
             self.action = self.change_animation(self.action, 1)
 
@@ -393,7 +173,6 @@ class Boss(Character):                      #ignore this class. It is not being 
         self.sprite = sprites[self.frame]
 
 
-        # self.rect = self.sprite.get_rect(topleft = (self.rect.x, self.rect.y))  #updates the rectangle of sprite
         self.mask = pygame.mask.from_surface(self.sprite) 
 
 
@@ -402,21 +181,95 @@ class Boss(Character):                      #ignore this class. It is not being 
         if current == desired:
             return current
         
-        elif self.stunned_cooldown == 0:
-            current = desired
-            self.frame = 0
+    
+        current = desired
+        self.frame = 0
 
         return current
-    
+
+
     def draw(self, window):
         
-        window.blit(self.animation[self.action][self.frame], (self.rect.x-180, self.rect.y - 200))
-        # window.blit(self.animation[self.action][self.frame], (self.rect.x, self.rect.y))
-
-
+        window.blit(self.animation[self.action][self.frame], (self.rect.x - self.offset[0], self.rect.y - self.offset[1]))
 
 
     
+
+class groundEnemies(Character):
+    def __init__(self, x, y):
+        self.currentSprite = "Walk"
+        self.direction = "left"
+        data = [160, 128, 3]
+        super().__init__(x, y, data)
+        super().loadImages("Characters", "Ground_Enemy", 150, 150, True)
+        self.rect = pygame.Rect(x, y, 90, 150)
+
+
+    
+    def update_sprite(self):
+        if self.health > 0 and self.attacking == False:
+            self.running = True
+            self.currentSprite = "Walk"
+        elif self.attacking == True:
+            self.currentSprite = "Attack"
+
+        image_sheet_name =  self.currentSprite + "_" + self.direction
+        sprites = self.character[image_sheet_name]
+
+        
+        if pygame.time.get_ticks() - self.timeUpdate > self.cooldown:       #changes frame based on a how much time has passed
+            self.timeUpdate = pygame.time.get_ticks()
+            self.frame += 1
+        
+        if self.frame >= len(self.character[image_sheet_name]):     #once the frame goes through the last frame of the current action spritesheet, it would start again in the beginning. This to prevent the frame from going out of bounds 
+            if self.health == 0:
+                self.Alive = False
+            self.frame = 0
+
+        self.sprite = sprites[self.frame]
+
+
+    def draw(self, window):
+        window.blit(self.sprite, (self.rect.x - 155, self.rect.y - 155))
+
+
+class flyingEnemies(Character):
+    def __init__(self, x, y):
+        self.currentSprite = "Flight"
+        self.direction = "left"
+        data = [160, 128, 3]
+        super().__init__(x, y, data)
+        super().loadImages("Characters", "Flying_Enemy", 150, 150, True)
+        self.rect = pygame.Rect(x, y, 100, 100)
+
+    def update_sprite(self):
+        if self.health > 0 and self.attacking == False:
+            self.running = True
+            self.currentSprite = "Flight"
+        elif self.attacking == True:
+            self.currentSprite = "Attack"
+
+        image_sheet_name = self.currentSprite + "_" + self.direction
+        sprites = self.character[image_sheet_name]
+
+        
+        if pygame.time.get_ticks() - self.timeUpdate > self.cooldown:       #changes frame based on a how much time has passed
+            self.timeUpdate = pygame.time.get_ticks()
+            self.frame += 1
+        
+        if self.frame >= len(self.character[image_sheet_name]):     #once the frame goes through the last frame of the current action spritesheet, it would start again in the beginning. This to prevent the frame from going out of bounds 
+            if self.health == 0:
+                self.Alive = False
+            self.frame = 0
+
+        self.sprite = sprites[self.frame]
+
+    def draw(self, window):
+        window.blit(self.sprite, (self.rect.x - 160, self.rect.y - 175))
+
+
+
+
 
 
 
